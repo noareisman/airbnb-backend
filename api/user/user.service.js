46506418fca9fd29eed236288609a2e52632d218
Your Name <your_address_email@domain.com>
2021-03-22 16:18:28 +0200

    login

--- api/user/user.service.js
+++ api/user/user.service.js
@@ -78,7 +78,6 @@ async function update(user) {
             _id: ObjectId(user._id),
             username: user.username,
             fullname: user.fullname,
-            score: user.score
         }
         const collection = await dbService.getCollection('user')
         await collection.updateOne({ '_id': userToSave._id }, { $set: userToSave })
@@ -96,7 +95,8 @@ async function add(user) {
             username: user.username,
             password: user.password,
             fullname: user.fullname,
-            score: user.score || 0
+            imgUrl: user.imgUrl ||' https://img.17qq.com/images/hrwssasuuax.jpeg',
+            messages: {}
         }
         const collection = await dbService.getCollection('user')
         await collection.insertOne(userToAdd)
